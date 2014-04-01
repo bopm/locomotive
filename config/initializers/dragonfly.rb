67779045374a3a6461a8e9ffac87219a03b1cd8f
Sergey Moiseev <moiseev.sergey@gmail.com>
2014-04-01 22:14:27 +0400

    initial commit

---
+++ config/initializers/dragonfly.rb
@@ -0,0 +1,24 @@
+require 'dragonfly'
+
+# Configure
+Dragonfly.app.configure do
+  plugin :imagemagick,
+    convert_command:  `which convert`.strip.presence || '/usr/local/bin/convert',
+    identify_command: `which identify`.strip.presence || '/usr/local/bin/identify'
+
+  protect_from_dos_attacks true
+
+  secret '6db34b0eea04538573e60f2cc20ce8be67814b1d1950661a2181524f0b4035ac'
+
+  url_format '/images/dynamic/:job/:basename.:ext'
+
+  fetch_file_whitelist /public/
+
+  fetch_url_whitelist /.+/
+end
+
+# Logger
+Dragonfly.logger = Rails.logger
+
+# Mount as middleware
+Rails.application.middleware.use Dragonfly::Middleware
