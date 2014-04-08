1fd44df37d2ea657b5edf187b02e3f804eb835c9
Sergey Moiseev <moiseev.sergey@gmail.com>
2014-04-08 19:53:53 +0400

    config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,15 +10,8 @@ CarrierWave.configure do |config|
 
   when :production
     # the following configuration works for Amazon S3
-    config.storage          = :fog
-    config.fog_credentials  = {
-      provider:                 'AWS',
-      aws_access_key_id:        ENV['S3_KEY_ID'],
-      aws_secret_access_key:    ENV['S3_SECRET_KEY'],
-      region:                   ENV['S3_BUCKET_REGION']
-    }
-    config.fog_directory    = ENV['S3_BUCKET']
-
+    config.storage = :file
+    config.root = File.join(Rails.root, 'public')
   else
     # settings for the local filesystem
     config.storage = :file
